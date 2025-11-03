fe1f14b6a9be8dac6cdf7eaa20ea4a620421e8a3
JazzAnt <jason.kwee98@gmail.com>
2025-11-02 23:14:59 -0800

    change database instantiating to a synchronized companion object so that it doesn't create a value and also doesn't attempt to create multiple instances

--- app/src/main/java/com/jazzant/expensetracker/ExpenseDatabase.kt
+++ app/src/main/java/com/jazzant/expensetracker/ExpenseDatabase.kt
@@ -13,6 +13,21 @@ import java.util.Date
 @TypeConverters(DateConverters::class)
 abstract class ExpenseDatabase : RoomDatabase(){
     abstract fun expenseDao(): ExpenseDao
+
+    companion object {
+        private var INSTANCE: ExpenseDatabase? = null
+        fun getInstance(context: Context): ExpenseDatabase {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context = context.applicationContext,
+                    klass = ExpenseDatabase::class.java,
+                    name = "expense_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
 
 class DateConverters {
--- app/src/main/java/com/jazzant/expensetracker/ExpenseViewModel.kt
+++ app/src/main/java/com/jazzant/expensetracker/ExpenseViewModel.kt
@@ -10,17 +10,12 @@ import kotlinx.coroutines.launch
 
 
 class ExpenseViewModel(): ViewModel() {
-    lateinit var expenseDatabase: ExpenseDatabase
     lateinit var expenseRepository: ExpenseRepository
     private val _expenseList = MutableStateFlow<List<Expense>>(emptyList())
     val expenseList: StateFlow<List<Expense>> get() = _expenseList
 
     fun setDatabase(context: Context){
-        expenseDatabase = Room.databaseBuilder(
-            context,
-            ExpenseDatabase::class.java,
-            "expense_database"
-        ).build()
+        val expenseDatabase = ExpenseDatabase.getInstance(context)
         expenseRepository = ExpenseRepository(expenseDatabase.expenseDao())
         viewModelScope.launch {
             expenseRepository.getAllExpenses().collect { expenses ->
