424f28a73754372a1e0a8d2d7b86470f851a3b29
JazzAnt <jason.kwee98@gmail.com>
2025-11-09 00:32:36 -0800

    modify lazycolumn elements to be clickable

--- app/src/main/java/com/jazzant/expensetracker/ExpenseListScreen.kt
+++ app/src/main/java/com/jazzant/expensetracker/ExpenseListScreen.kt
@@ -1,5 +1,7 @@
 package com.jazzant.expensetracker
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,27 +13,29 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ExpenseListScreen(list: List<Expense>){
-    Text("Expenses:")
-    LazyColumn (
-    ) {
+fun ExpenseListScreen(list: List<Expense>, onCardClick: (Expense) -> Unit){
+    LazyColumn () {
         items(list){
-                item ->
-            ExpenseCard(item)
+            item ->
+            ExpenseCard(item, onCardClick)
         }
-
     }
 }
 @Composable
-fun ExpenseCard(expense: Expense){
+fun ExpenseCard(expense: Expense, onCardClick: (Expense)->Unit){
     Card (
-        modifier = Modifier.fillMaxWidth().padding(start = 5.dp, end = 5.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .border(width = 1.dp, color = Color.Black),
+        onClick = {onCardClick(expense)}
     ){
         Row (
-            modifier = Modifier.fillMaxWidth().padding(start = 5.dp, end = 5.dp)
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
         ) {
             Column (
                 modifier = Modifier.fillMaxWidth(0.5f)
