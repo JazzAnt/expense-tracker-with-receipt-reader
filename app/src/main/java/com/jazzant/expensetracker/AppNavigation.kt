fc545f3d1c2fb2e6cf9c3895ab6f0c203d96eee7
JazzAnt <jason.kwee98@gmail.com>
2025-11-09 19:35:40 -0800

    add topbar (currently no function besides aestetics)

--- app/src/main/java/com/jazzant/expensetracker/AppNavigation.kt
+++ app/src/main/java/com/jazzant/expensetracker/AppNavigation.kt
@@ -1,19 +1,26 @@
 package com.jazzant.expensetracker
 
 import android.content.Context
+import android.graphics.drawable.Icon
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -46,6 +53,11 @@ fun ExpenseApp(
         backStackEntry?.destination?.route?: AppScreen.EXPENSE_LIST.name
     )
     Scaffold(
+        topBar = {
+            TopNavBar(
+                currentRoute = currentScreen.name
+            )
+        },
         bottomBar = {
             BottomNavBar(
                 items = listOf(
@@ -115,6 +127,31 @@ fun ExpenseApp(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopNavBar(
+    currentRoute: String,
+    modifier: Modifier = Modifier
+){
+    TopAppBar(
+        title = { Text(currentRoute) },
+        modifier = modifier,
+        navigationIcon = {
+
+            IconButton(
+                onClick = {
+                },
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Home",
+                    tint = Color.Gray
+                )
+            }
+        }
+    )
 }
 
 data class BottomNavItem(
