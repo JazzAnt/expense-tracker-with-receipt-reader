cc1911e5b5b0f6be76d63b17e0c572ac59fe7dc6
JazzAnt <jason.kwee98@gmail.com>
2025-11-10 11:30:11 -0800

    add gradle dependencies for CameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,12 @@ dependencies {
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
     implementation("androidx.navigation:navigation-compose:2.9.5")
+    val camerax_version = "1.5.1"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
 }
