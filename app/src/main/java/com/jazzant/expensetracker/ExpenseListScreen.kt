d72cee05f74a873111132c9e8f0b3ac0bf846d12
Januar <januarj@student.douglascollege.ca>
2025-11-10 17:38:34 -0800

    add composable to display the sum of expenses (currently static value)

--- app/src/main/java/com/jazzant/expensetracker/ExpenseListScreen.kt
+++ app/src/main/java/com/jazzant/expensetracker/ExpenseListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,22 +22,46 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.jazzant.expensetracker.ui.theme.ExpenseTrackerWithBillReaderTheme
 import java.text.SimpleDateFormat
 import java.util.Date
 
 @Composable
 fun ExpenseListScreen(list: List<Expense>, onCardClick: (Expense) -> Unit){
-    LazyColumn () {
-        items(list){
-            item ->
-            ExpenseCard(item, onCardClick)
+    Box(Modifier.fillMaxSize()){
+        LazyColumn () {
+            items(list){
+                    item ->
+                ExpenseCard(item, onCardClick)
+            }
+        }
+        ExpenseSumCard(100f, Modifier.align(Alignment.BottomEnd))
+    }
+}
+
+@Composable
+fun ExpenseSumCard(sum: Float, modifier: Modifier = Modifier){
+    Card(modifier
+        .padding(2.dp)
+        .border(BorderStroke(1.dp, Color.Black))
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(50.dp)
+        .zIndex(1f)) {
+        Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
+            Text("Sum: $%.2f".format(sum),
+                fontSize = TextUnit(5f, TextUnitType.Em),
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
