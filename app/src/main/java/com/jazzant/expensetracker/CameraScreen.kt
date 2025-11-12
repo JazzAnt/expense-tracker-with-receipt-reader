26f0936b55f0dfef083a5d0673caf56926d527b4
JazzAnt <jason.kwee98@gmail.com>
2025-11-11 22:56:54 -0800

    rework CameraPreviewScreen composable (notably now no longer uses coroutines)

--- app/src/main/java/com/jazzant/expensetracker/CameraScreen.kt
+++ app/src/main/java/com/jazzant/expensetracker/CameraScreen.kt
@@ -3,7 +3,6 @@ package com.jazzant.expensetracker
 import android.content.Context
 import android.media.Image
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.OptIn
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ExperimentalGetImage
@@ -17,102 +16,94 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import java.util.concurrent.Executors
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 @Composable
-fun CameraPreviewScreen(textRecognitionVM: TextRecognitionVM = viewModel()){
-    val lensFacing = CameraSelector.LENS_FACING_BACK
+fun CameraPreviewScreen(modifier:Modifier = Modifier, onImageCapture: (ImageProxy)-> Unit){
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
-    //Build the preview object
-    val preview = Preview.Builder()
-        .build()
-    val previewView = remember {
-        PreviewView(context)
-    }
-    //Build the camera selector, require back facing lens
-    //Using CameraSelector.DEFAULT_BACK_CAMERA also works
-    val cameraxSelector = CameraSelector.Builder()
-        .requireLensFacing(lensFacing)
-        .build()
+    val previewView = remember { PreviewView(context) }
+    var imageCapture: ImageCapture? by remember { mutableStateOf(null) }
 
-    val imageCapture: ImageCapture? by remember { mutableStateOf(null) }
+    Box(modifier = Modifier.padding(bottom = 50.dp)) {
+        AndroidView(
+            factory = { previewView },
+            modifier = modifier
+        ) { view -> //this is the block provided to the factory, in this case previewView
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build()
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
-    //Launch Camera as a coroutine
-    LaunchedEffect(lensFacing) {
-        //Get the camera provider from the context
-        val cameraProvider = context.getCameraProvider()
-        //Unbind existing use cases before rebinding
-        cameraProvider.unbindAll()
-        //Bind use cases to camera
-        cameraProvider.bindToLifecycle(
-            lifecycleOwner = lifecycleOwner,
-            cameraSelector = cameraxSelector,
-            preview,
-            imageCapture
-            )
-        //The preview Surface property is received from the PreviewView
-        preview.surfaceProvider = previewView.surfaceProvider
-    }
-    //Turns the PreviewView (an android View) into a composable
-    Box(
-        contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()
-    ){
-        AndroidView(factory = {previewView}, modifier = Modifier.fillMaxSize())
-        Column {
+                imageCapture = ImageCapture.Builder().build()
+                preview.surfaceProvider = view.surfaceProvider
 
-        Card {
-            Text("Text:\n${textRecognitionVM.recognizedText.value}")
+                /* Bind the preview and imageCapture to the cameraProvider use cases
+                 * Basically tells the cameraProvider what values we want from it
+                 * In this case: Preview to show the camera view on our composable
+                 * And imageCapture to capture the image and store the value as ImageProxy
+                 */
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner = lifecycleOwner,
+                        cameraSelector = cameraSelector,
+                        preview,
+                        imageCapture
+                    )
+                } catch (e: Exception) {
+                    Log.e("CameraScreen", "Camera Binding Failed: ${e.message}")
+                }
+            }, ContextCompat.getMainExecutor(context))
         }
-        Button(
+        FloatingActionButton(
             onClick = {
                 imageCapture?.takePicture(
                     ContextCompat.getMainExecutor(context),
                     object : ImageCapture.OnImageCapturedCallback(){
-                        @OptIn(ExperimentalGetImage::class)
-                        override fun onCaptureSuccess(image: ImageProxy) {
-                            val mediaImage = image.image
-                            if(mediaImage != null) {
-                                textRecognitionVM.recognizeText(
-                                    mediaImage,
-                                    image.imageInfo.rotationDegrees
-                                )
-                            }
-                            image.close()
+                        override fun onCaptureSuccess(imageProxy: ImageProxy) {
+                            onImageCapture(imageProxy)
+                            imageProxy.close()
                         }
 
                         override fun onError(exception: ImageCaptureException) {
-
+                            Log.e("CameraScreen", "Image Capture Failed: ${exception.message}")
                         }
                     }
                 )
-
-            }
-        ) { Text("Capture Image") }
+            },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            Icon(Icons.Default.Search, contentDescription = "Take Picture to Analyze")
         }
-
     }
 }
 
@@ -134,11 +125,11 @@ class TextRecognitionVM: ViewModel(){
     }
 }
 
-private class TextAnalyzer : ImageAnalysis.Analyzer{
+private class TextAnalyzer : ImageAnalysis.Analyzer {
     @OptIn(ExperimentalGetImage::class)
     override fun analyze(imageProxy: ImageProxy) {
         val mediaImage = imageProxy.image
-        if(mediaImage != null){
+        if (mediaImage != null) {
             val inputImage = InputImage.fromMediaImage(
                 mediaImage,
                 imageProxy.imageInfo.rotationDegrees
@@ -146,7 +137,7 @@ private class TextAnalyzer : ImageAnalysis.Analyzer{
             val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
             val result = recognizer.process(inputImage)
                 .addOnSuccessListener { visionText ->
-                    for (block in visionText.textBlocks){
+                    for (block in visionText.textBlocks) {
                         Log.d("TEXTREADER", block.text)
                     }
                 }
@@ -155,20 +146,4 @@ private class TextAnalyzer : ImageAnalysis.Analyzer{
                 }
         }
     }
-
 }
-
-private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
-    suspendCoroutine { continuation ->
-        //Get instance of CameraProvider from Context to bind lifecycle of cameras to owner
-        //This means there's no need to open and close the camera manually
-        //CameraX will automatically do that depending on the lifecycle
-        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
-            cameraProvider.addListener({
-                //Bind Lifecycle of Cameras to Lifecycle Owner (Context)
-                continuation.resume(cameraProvider.get())
-            }, ContextCompat.getMainExecutor(this))
-                //This returns an executor that runs on the main thread
-                //Thus the runnable task is run there
-        }
-    }
