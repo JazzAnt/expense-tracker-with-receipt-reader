21dcab0d388520d0b0642ea83eccc0a2ec17e157
Jason A. Januar <jason.kwee98@gmail.com>
2025-11-19 00:37:53 -0800

    create ChooseAmountScreen

---
+++ app/src/main/java/com/jazzant/expensetracker/screens/ChooseAmountScreen.kt
@@ -0,0 +1,43 @@
+package com.jazzant.expensetracker.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.jazzant.expensetracker.R
+import com.jazzant.expensetracker.ui.RadioButtons
+
+@Composable
+fun ChooseAmountScreen(
+    amountList: List<String>,
+    amount: String,
+    onAmountChange: (String) -> Unit,
+    onNextButtonPress: () -> Unit,
+    invalidInput: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.fillMaxSize()) {
+        Text(stringResource(R.string.chooseAmount_header))
+        Text(stringResource(R.string.chooseAmount_description))
+        RadioButtons(
+            label = stringResource(R.string.chooseAmount_radioButtonLabel),
+            radioOptions = amountList,
+            selectedOption = amount,
+            onOptionChange = onAmountChange,
+        )
+        if (invalidInput)
+        {
+            Text(stringResource(R.string.chooseAmount_invalidAmountLabel))
+        }
+        else
+        {
+            Button(onClick = onNextButtonPress) {
+                Text(stringResource(R.string.nextButton))
+            }
+        }
+    }
+}
