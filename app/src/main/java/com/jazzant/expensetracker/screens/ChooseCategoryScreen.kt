b9225db5daf928beddb4bd5ce2ff5b952cd48821
Jason A. Januar <jason.kwee98@gmail.com>
2025-11-26 03:02:20 -0800

    created choose category screen
    like all screens this is just a bare bones functional screen with no proper UI design

---
+++ app/src/main/java/com/jazzant/expensetracker/screens/ChooseCategoryScreen.kt
@@ -0,0 +1,71 @@
+package com.jazzant.expensetracker.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.jazzant.expensetracker.R
+import com.jazzant.expensetracker.ui.RadioButtons
+import com.jazzant.expensetracker.ui.SwitchField
+import com.jazzant.expensetracker.ui.TextInput
+
+@Composable
+fun ChooseCategoryScreen(
+    newCategorySwitch: Boolean,
+    onNewCategorySwitchChange: (Boolean) -> Unit,
+    category: String,
+    onCategoryChange: (String) -> Unit,
+    categoryList: List<String>,
+    onNextButtonPress: () -> Unit,
+    invalidInput: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.fillMaxSize()) {
+        Text(stringResource(R.string.chooseCategory_header))
+        Text(stringResource(R.string.chooseCategory_description))
+
+        SwitchField(
+            text = stringResource(R.string.newCategorySwitchLabel),
+            state = newCategorySwitch,
+            onStateChanged = onNewCategorySwitchChange
+        )
+        if (newCategorySwitch)
+        {
+            TextInput(
+                label = stringResource(R.string.newCategoryInputLabel),
+                value = category,
+                onValueChange = onCategoryChange,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        else if (categoryList.isEmpty())
+        {
+            Text(stringResource(R.string.emptyCategoryListAlert))
+        }
+        else
+        {
+            RadioButtons(
+                label = stringResource(R.string.categorySelectorLabel),
+                radioOptions = categoryList,
+                selectedOption = category,
+                onOptionChange = onCategoryChange,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        if (invalidInput)
+        {
+            Text(stringResource(R.string.chooseCategory_invalidAmountLabel))
+        }
+        else
+        {
+            Button(onClick = onNextButtonPress) {
+                Text(stringResource(R.string.nextButton))
+            }
+        }
+    }
+}
