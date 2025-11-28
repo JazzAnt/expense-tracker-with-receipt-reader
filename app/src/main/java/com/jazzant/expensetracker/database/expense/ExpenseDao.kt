bac5f7bd8e0a8bbaabe63ecf2606917469b47067
Jason A. Januar <jason.kwee98@gmail.com>
2025-11-28 00:08:39 -0800

    create overloaded functions for querying the Expense table that enables filtering based on category, dateRange, or name
    Name uses LIKE thus pattern matching can be used, while Category uses = so it needs to be an exact match. So for querying categories it should use a selector using values from getAllCategories()

--- app/src/main/java/com/jazzant/expensetracker/database/expense/ExpenseDao.kt
+++ app/src/main/java/com/jazzant/expensetracker/database/expense/ExpenseDao.kt
@@ -20,10 +20,26 @@ interface ExpenseDao {
 
     @Query("SELECT * FROM expense_table ORDER BY date DESC")
     fun getAllExpenses(): Flow<List<Expense>>
+    @Query("SELECT * FROM expense_table WHERE category=:category  ORDER BY date DESC")
+    fun getAllExpenses(category: String): Flow<List<Expense>>
+    @Query("SELECT * FROM expense_table WHERE date BETWEEN :startDate AND :endDate ORDER BY date DESC")
+    fun getAllExpenses(startDate: Long, endDate: Long): Flow<List<Expense>>
+    @Query("SELECT * FROM expense_table WHERE category=:category AND date BETWEEN :startDate AND :endDate ORDER BY date DESC")
+    fun getAllExpenses(category: String, startDate: Long, endDate: Long): Flow<List<Expense>>
+
+    @Query("SELECT * FROM expense_table WHERE LOWER(name) LIKE LOWER(:name)")
+    fun searchExpensesByName(name: String): Flow<List<Expense>>
 
     @Query("SELECT DISTINCT category FROM expense_table ORDER BY category ASC")
     fun getAllCategories(): Flow<List<String>>
 
     @Query("SELECT SUM(amount) FROM expense_table")
     fun getSumOfExpenses(): Flow<Float>
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category=:category")
+    fun getSumOfExpenses(category: String): Flow<Float>
+    @Query("SELECT SUM(amount) FROM expense_table WHERE date BETWEEN :startDate AND :endDate")
+    fun getSumOfExpenses(startDate: Long, endDate: Long): Flow<Float>
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category=:category AND date BETWEEN :startDate AND :endDate")
+    fun getSumOfExpenses(category: String, startDate: Long, endDate: Long): Flow<Float>
+
 }
