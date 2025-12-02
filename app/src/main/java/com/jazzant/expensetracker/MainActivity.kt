0991014b57d7b0b3c8254d1ea16a88ee91245cfa
Jason A. Januar <jason.kwee98@gmail.com>
2025-12-01 23:11:30 -0800

    add theme

--- app/src/main/java/com/jazzant/expensetracker/MainActivity.kt
+++ app/src/main/java/com/jazzant/expensetracker/MainActivity.kt
@@ -4,25 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.jazzant.expensetracker.ui.theme.ExpenseTrackerWithBillReaderTheme
+import com.jazzant.expensetracker.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExpenseTrackerWithBillReaderTheme {
+            AppTheme {
                 ExpenseApp()
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpenseTrackerWithBillReaderTheme {
     }
 }
